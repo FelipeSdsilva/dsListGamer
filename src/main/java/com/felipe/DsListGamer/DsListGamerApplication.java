79fd94010aa1773f3ce5232c900a46021ba85e15
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-08 22:05:41 -0300

    Resolvendo os conflitos

---
+++ src/main/java/com/felipe/DsListGamer/DsListGamerApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.DsListGamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListGamerApplication.class, args);
+	}
+
+}
