6c6f9e0f5155d9896296027c512c3833ac142a3d
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-20 20:02:37 -0300

    Criando método de movimentação e reposicionamento na lista

--- src/main/java/com/felipe/DsListGamer/services/GameListService.java
+++ src/main/java/com/felipe/DsListGamer/services/GameListService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.felipe.DsListGamer.dto.GameListDTO;
+import com.felipe.DsListGamer.projections.GameMinProjection;
 import com.felipe.DsListGamer.repositories.GameListRepository;
+import com.felipe.DsListGamer.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -15,9 +17,21 @@ public class GameListService {
 	@Autowired
 	private GameListRepository repository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> listAll(){
 		return repository.findAll().stream().map(GameListDTO::new).toList();
 	}
 
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		
+		list.add(destinationIndex, obj);
+		
+		
+	}
 }
